fe471cbf9340d5e423796be59b910dcdcef0fa1b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-05-08 13:13:45 +0200

    template of the widget in dashboard

---
+++ root/usr/share/nethesis/NethServer/Template/Dashboard/SystemStatus/Fail2ban.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<div class='dashboard-item'>";
+echo $view->header()->setAttribute('template',$T('fail2banstatus_title'));
+
+if ( ! file_exists ('/var/lib/nethserver/db/fail2ban.json')) {
+    echo $T('no_fail2ban_stat_label');
+    }
+else {
+    $totalCounter = 0;
+    foreach ($view['status']['TotalBannedIP'] as $key => $value) {
+        if (is_numeric ($value)){
+            $totalCounter = $totalCounter + $value;
+        }
+    }
+    echo '<br/>'.'<b>'.$T('TotalBannedIP').'</b>'." : $totalCounter";
+}
+
