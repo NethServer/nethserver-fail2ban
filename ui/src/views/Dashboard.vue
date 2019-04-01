9c2c23b7d2d4bc2a0a7852b3bc033f9e64518825
Stephane de Labrusse <stephdl@de-labrusse.fr>
2019-04-01 23:04:04 +0200

    fixup! Read api for dashboard statistic

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,101 +1,63 @@
 <template>
-  <div>
-    <h2>{{$t('dashboard.title')}}</h2>
-    <div v-if="!view.isLoaded" class="spinner spinner-lg"></div>
-    <div v-if="view.isLoaded">
-
-      <!-- <form class="form-horizontal" v-on:submit.prevent="unban()"> -->
-
-<!-- <div class="container">
-      <div class="row "> -->
-      <!-- <div class="container"> -->
-
-      <h3 class="col-lg-6">{{$t('fail2ban.number_of_enabled_jail')}}: {{configuration.JailStatus.length}}</h3>
-
-<!-- <h3>{{$t('fail2ban.statistic_per_jail')}}</h3>
-
-  <div  v-for="jail in configuration.JailStatus">
-
-      <div class="row">
-  <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">{{jail}}</div>
-  
-  </div>
- </div> -->
-
-
-
- <div class="row row-dashboard">
-   <div class="col-lg-8">
-     <!-- <h3>{{$t('fail2ban.statistic_per_jail')}}</h3> -->
-
-     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 resources-panel">
-       <div class="panel panel-default">
-         <div class="panel-heading">
-           <h3 class="panel-title">
-             <span class="icon-header-panel">
-               <span class="fa fa-check right"></span>
-           </span>{{$t('fail2ban.list_enabled_jails')}}
-           </h3>
-         </div>
-         <div class="panel-body">
-           <!-- <div id="ram-chart" class="text-center"></div> -->
-           <!-- <div class="text-right ">{{$t('dashboard.size')}}: -->
-               <div  v-for="jail in configuration.JailStatus">
-
-                   <div class="row">
-               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{jail}}</div>
-               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"></div>
-               
-               </div>
-              </div>
-           </div>
-         </div>
-       </div>
-       
-       <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 resources-panel">
-         <div class="panel panel-default">
-           <div class="panel-heading">
-             <h3 class="panel-title">
-               <span class="icon-header-panel">
-                 <span class="fa pficon-locked right"></span>
-               </span>{{$t('fail2ban.statistic_per_jail')}}
-             </h3>
-           </div>
-           <div class="panel-body">
-             <!-- <div id="ram-chart" class="text-center"></div> -->
-             <!-- <div class="text-right ">{{$t('dashboard.size')}}: -->
-             <div  v-for="(value, Jail) in configuration.TotalBannedIP">
-             <div class="row">
-              <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{Jail}}:</div>
-             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{value}}</div>
-             </div>
-             </div>
-             </div>
-           </div>
-         </div>
-       
-       
-       
-   </div>
-
+    <div>
+        <h2>{{$t('dashboard.title')}}</h2>
+        <div v-if="!view.isLoaded" class="spinner spinner-lg"></div>
+        <div v-if="view.isLoaded">
+            <h3 class="col-lg-6">{{$t('fail2ban.number_of_enabled_jail')}}: {{configuration.JailStatus.length}}</h3>
+            <div class="row row-dashboard">
+                <div class="col-lg-8"> 
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 resources-panel">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <span class="icon-header-panel">
+                                        <span class="fa fa-check right"></span>
+                                    </span>{{$t('fail2ban.list_enabled_jails')}}
+                                </h3>
+                            </div>
+                            <div class="panel-body">
+                                <div  v-for="jail in configuration.JailStatus">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{jail}}</div>
+                                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 resources-panel">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <span class="icon-header-panel">
+                                        <span class="fa pficon-locked right"></span>
+                                    </span>{{$t('fail2ban.statistic_per_jail')}}
+                                </h3>
+                            </div>
+                            <div class="panel-body">
+                                <div  v-for="(value, Jail) in configuration.TotalBannedIP">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{Jail}}:</div>
+                                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">{{value}}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <h3>{{$t('fail2ban.statistic_per_jail')}}</h3>
+            <div  v-for="(value, Jail) in configuration.TotalBannedIP">
+                div class="row">
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">{{Jail}}:</div>
+                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">{{value}}</div>
+                <div class="divider"></div>
+            </div>
+        </div> -->
+    </div>
 </div>
-
-  <!-- <div class="container">
-
-
-
-<h3>{{$t('fail2ban.statistic_per_jail')}}</h3>
-<div  v-for="(value, Jail) in configuration.TotalBannedIP">
-div class="row">
- <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">{{Jail}}:</div>
-<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">{{value}}</div>
- <div class="divider"></div>
-</div>
-</div> -->
-  </div>
-  </div>
-<!-- </div> -->
-
 </template>
 
 <script>
